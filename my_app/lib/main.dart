0c358ad133e06b52f6ef5dc67558b12bb1290229
GitSRealpe <msn-realpe@hotmail.com>
2026-04-11 22:06:36 +0200

    styling

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -16,7 +16,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: const Color.fromARGB(255, 19, 58, 129),
+        ),
+      ),
       home: const HomePage(title: 'GSOC26 AGL-Quiz Apsp'),
     );
   }
@@ -35,7 +39,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
   late final AudioPlayer _audioPlayer;
 
   final deviceInfo = DeviceInfoPlugin();
@@ -65,12 +68,6 @@ class _HomePageState extends State<HomePage> {
     super.dispose();
   }
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   // Navigate to the new Image Screen
   void _openImageScreen() {
     Navigator.push(
@@ -88,81 +85,119 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Center(child: Text(widget.title)),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: .center,
-          children: [
-            const Text('Captain GitSRealpe:'),
-            Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(text: "OS Name: "),
-                  TextSpan(
-                    text: _osRelease.name,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            Text.rich(
-              TextSpan(
-                children: [
-                  TextSpan(text: "Version: "),
-                  TextSpan(
-                    text: _osRelease.version,
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Expanded(child: const SizedBox(width: 40)),
-
-            // image screen button
-            Row(
-              mainAxisAlignment: .center,
-              children: [
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: _openImageScreen,
-                      child: Column(
-                        children: [
-                          Icon(Icons.videocam, size: widget.iconSize),
-                          const Text('Ship views'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: _playHonk,
-                      child: Column(
-                        children: [
-                          Icon(Icons.volume_up, size: widget.iconSize),
-                          const Text('Honk'),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+        backgroundColor: const Color.fromARGB(255, 3, 12, 87),
+        title: Center(
+          child: Text(widget.title, style: TextStyle(color: Colors.white)),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 23, 65, 119),
+            image: const DecorationImage(
+              image: AssetImage('assets/ship_overview.png'),
+              fit: BoxFit.none,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              const Text(
+                'Captain: GitSRealpe',
+                style: TextStyle(color: Colors.white, fontSize: 32),
+              ),
+              Expanded(child: const SizedBox(width: 40)),
+              const Text(
+                'Ship systems running on:',
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "OS Name: ",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                    TextSpan(
+                      text: _osRelease.name,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Version: ",
+                      style: TextStyle(fontSize: 20, color: Colors.white),
+                    ),
+                    TextSpan(
+                      text: _osRelease.version,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              // image screen button
+              Row(
+                mainAxisAlignment: .center,
+                children: [
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: _openImageScreen,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            163,
+                            214,
+                            255,
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Icon(Icons.videocam, size: widget.iconSize),
+                            const Text('Ship rear view'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: _playHonk,
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            163,
+                            214,
+                            255,
+                          ),
+                        ),
+                        child: Column(
+                          children: [
+                            Icon(Icons.volume_up, size: widget.iconSize),
+                            const Text('Honk'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
@@ -179,9 +214,14 @@ class ImageScreen extends StatelessWidget {
         title: const Text("Image View"),
         backgroundColor: Colors.green,
       ),
-      body: Center(
-        child: InteractiveViewer(
-          child: Image.asset("assets/rear_view.png", fit: BoxFit.contain),
+      body: Container(
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 23, 65, 119),
+        ),
+        child: Center(
+          child: InteractiveViewer(
+            child: Image.asset("assets/rear_view.png", fit: BoxFit.contain),
+          ),
         ),
       ),
     );
